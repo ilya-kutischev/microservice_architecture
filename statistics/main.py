188c93890b957d0c19fa54f203ad9c5563ec60a9
root <kutischev10@gmail.com>
2022-09-07 16:44:03 +0300

    almost done connection to mongo

--- statistics/main.py
+++ statistics/main.py
@@ -10,6 +10,27 @@ from db_models import DataModel
 app = FastAPI()
 
 
+@app.post("/add_stats", response_description="Add new student")
+async def add_datasearch(datasearch):
+    MONGO_DETAILS = "mongodb://root:root@mongodb:27017"
+
+    client = motor.motor_asyncio.AsyncIOMotorClient(
+        io_loop=asyncio.get_running_loop(),
+        host="mongodb",
+        port=27017,
+        username="root",
+        password="root",
+    )
+
+    db = client.statistics
+
+    stats_collection = db.get_collection("statistics")
+    datasearch = await db["statistics"].insert_one(datasearch)
+    # Selecting instance
+    added_datasearch = await db["statistics"].find_one({"_id": datasearch.inserted_id})
+    print(f"Added New {added_datasearch}")
+    return JSONResponse(status_code=status.HTTP_201_CREATED, content=added_datasearch)
+
 async def kafka_consumer(loop):
     # #KAFKA CONSUMER =====================================================
     # global consumer
@@ -26,6 +47,13 @@ async def kafka_consumer(loop):
                     msg.topic, msg.partition, msg.offset, msg.key, msg.value,
                     msg.timestamp)
             )
+            datasearch ={
+                "datasearch": msg.value,
+                "count": "1"
+            }
+
+            loop = asyncio.get_running_loop()
+            loop.create_task(add_datasearch(datasearch))
     finally:
         await consumer.stop()
 
@@ -33,10 +61,18 @@ async def kafka_consumer(loop):
 @app.on_event("startup")
 async def _startup_event():
     await sleep(15)
-    MONGO_DETAILS = "mongodb://127.0.0.1:27017"
-    client = motor.motor_asyncio.AsyncIOMotorClient('localhost', 27017)
+    MONGO_DETAILS = "mongodb://root:root@mongodb:27017"
+
+    client = motor.motor_asyncio.AsyncIOMotorClient(
+        host="mongodb",
+        port=27017,
+        username="root",
+        password="root",
+        authSource="admin"
+    )
+
     global db
-    db = client.stats
+    db = client.statistics
     stats_collection = db.get_collection("statistics")
     # print(stats_collection)
 
@@ -49,10 +85,3 @@ async def read_root() -> dict:
     return {"message": "Welcome to our service"}
 
 
-@app.post("/add_stats", response_description="Add new student")
-async def add_datasearch(student: DataModel = Body(...)):
-    datasearch = await db["statistics"].insert_one(student)
-
-    # Selecting instance
-    created_student = await db["statistics"].find_one({"_id": datasearch.inserted_id})
-    return JSONResponse(status_code=status.HTTP_201_CREATED, content=created_student)
